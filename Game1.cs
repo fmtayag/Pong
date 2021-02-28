5ef083fa4d32cf88b2963bd2098d0e7736bc6251
zyenapz <zyenapz@gmail.com>
2021-02-28 19:31:57 +0800

    Added paddle

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -6,12 +7,10 @@ namespace Pong
 {
     public class Game1 : Game
     {
-        Texture2D ballTexture;
-        Vector2 ballPosition;
-        float ballSpeed;
-
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private List<Sprite> _sprites;
 
         public Game1()
         {
@@ -23,8 +22,6 @@ namespace Pong
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
-            ballSpeed = 100f;
 
             base.Initialize();
         }
@@ -32,9 +29,17 @@ namespace Pong
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            Texture2D playerTexture = Content.Load<Texture2D>("Paddle");
+            _sprites = new List<Sprite>()
+            {
+                new Player(playerTexture)
+                {
+                    Position = new Vector2(100,100),
+                    Color = Color.Blue,
+                    Speed = 300f
+                }
+            };
             // TODO: use this.Content to load your game content here
-            ballTexture = Content.Load<Texture2D>("ball");
         }
 
         protected override void Update(GameTime gameTime)
@@ -43,37 +48,19 @@ namespace Pong
                 Exit();
 
             // TODO: Add your update logic here
-            var kstate = Keyboard.GetState();
-
-            if (kstate.IsKeyDown(Keys.Up))
-                ballPosition.Y -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            if (kstate.IsKeyDown(Keys.Down))
-                ballPosition.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            if (kstate.IsKeyDown(Keys.Left))
-                ballPosition.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            if (kstate.IsKeyDown(Keys.Right))
-                ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            if (ballPosition.X > _graphics.PreferredBackBufferWidth - ballTexture.Width / 2)
-                ballPosition.X = _graphics.PreferredBackBufferWidth - ballTexture.Width / 2;
-            else if (ballPosition.X < ballTexture.Width / 2)
-                ballPosition.X = ballTexture.Width / 2;
-
-            if (ballPosition.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height / 2)
-                ballPosition.Y = _graphics.PreferredBackBufferHeight - ballTexture.Height / 2;
-            else if (ballPosition.Y < ballTexture.Height / 2)
-                ballPosition.Y = ballTexture.Height / 2;
-
+            foreach (Sprite sprite in _sprites)
+                sprite.Update(gameTime, _sprites); 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            _spriteBatch.Draw(ballTexture, ballPosition, null, Color.White, 0f, new Vector2(ballTexture.Width / 2, ballTexture.Height / 2), Vector2.One, SpriteEffects.None, 0f);
+            foreach (Sprite sprite in _sprites)
+                sprite.Draw(_spriteBatch);
             _spriteBatch.End();
 
             base.Draw(gameTime);
---
+++ Player.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace Pong
+{
+    public class Player : Sprite
+    {
+        public Player(Texture2D texture) : base(texture)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime, List<Sprite> sprites)
+        {
+            Move(gameTime);
+
+            base.Update(gameTime, sprites);
+        }
+
+        private void Move(GameTime gameTime)
+        {
+            var kstate = Keyboard.GetState();
+
+            if (kstate.IsKeyDown(Keys.Up))
+                Position.Y -= Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (kstate.IsKeyDown(Keys.Down))
+                Position.Y += Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (kstate.IsKeyDown(Keys.Left))
+                Position.X -= Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (kstate.IsKeyDown(Keys.Right))
+                Position.X += Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+    }
+
+}
